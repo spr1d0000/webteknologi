be809534259ce69978471d890d433aecec418215
Lukman Hakim <plangaplongo214@gmail.com>
2024-11-18 19:28:44 +0700

    first commit

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,13 +1,21 @@
+'use client'; //Penggunaan useActionState harus mencantumkan ini
+
 import { lusitana } from '@/app/ui/fonts';
 import Image from 'next/image';
 import {
     AtSymbolIcon,
     KeyIcon,
+    ExclamationCircleIcon,
 } from '@heroicons/react/24/outline';
 import { ArrowRightIcon } from '@heroicons/react/20/solid';
 import { Button } from '@/app/ui/button';
-
+import { useActionState } from 'react';
+import { authenticate } from '../lib/action'; //autentikasi
 export default function Page() {
+    const [errorMessage, formAction, isPending] = useActionState(
+        authenticate,
+        undefined,
+    );
     return (
         <main className="flex min-h-screen flex-col p-6">
             <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
@@ -20,7 +28,7 @@ export default function Page() {
                 />
             </div>
             <div className='flex flex-col justify-center gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20 m-auto'>
-                <form className='space-y-3'>
+                <form action={formAction} className='space-y-3'>
                     <div className="flex-1 rounded-lg bg-gray-50 px-6 pb-4 pt-8">
                         <h1 className={`${lusitana.className} mb-3 text-2xl`}>
                             Silahkan Masukan Account Anda.
@@ -70,6 +78,20 @@ export default function Page() {
                     </button>
                     <div className='flex h-8 items-end space-x-1'>
                         {/* add form errors here */}
+                        <Button className="mt-4 w-full" aria-disabled={isPending}>
+                            Masuk <ArrowRightIcon className="ml-auto h-5 w-5 text-gray-50"/>
+                        </Button>
+                        <div className="flex h=8 items-end space-x-1">
+                            {errorMessage && (
+                                <>
+                                <ExclamationCircleIcon className="h-5 w-5 text-red-500"/>
+                                <p className="text-sm text-red-500">{errorMessage}</p>
+                                </>
+                            )}
+
+                            
+                            
+                        </div>
                     </div>
                 </form>
             </div>
