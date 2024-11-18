48a17ae4853598bdcb585bfb1ce16edacc6695a9
Lukman Hakim <plangaplongo214@gmail.com>
2024-11-18 21:27:57 +0700

    update commit

--- app/lib/placeholder-data.ts
+++ app/lib/placeholder-data.ts
@@ -4,7 +4,7 @@ const users = [
   {
     id: '410544b2-4001-4271-9855-fec4b6a6442a',
     name: 'User',
-    email: 'dosen@nextmail.com',
+    email: 'user@nextmail.com',
     password: '123456',
   },
 ];
