4fcd2a8774990cb84756e21dd27fb1cc9f8d2c25
Lukman Hakim <plangaplongo214@gmail.com>
2024-11-21 23:05:08 +0700

    update commit

--- app/lib/action.ts
+++ app/lib/action.ts
@@ -6,7 +6,7 @@ import { AuthError } from 'next-auth';
 // ...
 
 export async function authenticate(
-prevState: string | undefined,
+    _prevState: string | undefined,
 formData: FormData,
 ) {
 try {
